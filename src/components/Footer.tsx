c1f3e498e5529f1d4135145262b09bf51fb0eb87
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2025-10-15 21:11:08 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,8 @@
-import { fetchSetting } from "@/lib/nezha-api"
-import { useQuery } from "@tanstack/react-query"
 import React from "react"
 import { useTranslation } from "react-i18next"
 
 const Footer: React.FC = () => {
   const { t } = useTranslation()
-
-  const { data: settingData } = useQuery({
-    queryKey: ["setting"],
-    queryFn: () => fetchSetting(),
-    refetchOnMount: true,
-    refetchOnWindowFocus: true,
-  })
 
   return (
     <footer className="mx-auto w-full max-w-5xl px-4 lg:px-0 pb-4 server-footer">
@@ -21,10 +12,8 @@ const Footer: React.FC = () => {
             &copy;2020-{new Date().getFullYear()}
           </div>
           <div className="server-footer-theme flex flex-col items-center sm:items-end">
-            {/* 移除了快捷键提示部分 */}
             <section>
               {t("footer.themeBy")}
-              {/* 移除了 nezha-dash 链接和 Git 哈希部分 */}
             </section>
           </div>
         </section>
